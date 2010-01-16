64ccc115118a38f7b37bb6b91824d05d5d43223f
tardate <gallagher.paul@gmail.com>
2010-01-16 22:27:39 +0800

    tidy

--- app/controllers/sales_by_customer_reports_controller.rb
+++ app/controllers/sales_by_customer_reports_controller.rb
@@ -1,12 +1,13 @@
 class SalesByCustomerReportsController < ApplicationController
-  def index
-      @report = ActiveWarehouse::Report::TableReport.new
-	@report.title =  "Sales by Customer"
-	@report.cube_name =  :sales_by_customer
-	@report.column_dimension_name = :date
-	@report.row_dimension_name = :customer
 
-	@report_view = @report.view( params, :with_totals => true)
-  end
+	def index
+		@report = ActiveWarehouse::Report::TableReport.new
+		@report.title =  "Sales by Customer"
+		@report.cube_name =  :sales_by_customer
+		@report.column_dimension_name = :date
+		@report.row_dimension_name = :customer
+
+		@report_view = @report.view( params, :with_totals => true)
+	end
 
 end
